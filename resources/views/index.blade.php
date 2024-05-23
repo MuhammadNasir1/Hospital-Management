29267cb7a86126dad088ca8c1d60125a3a66271d
mehran02111 <charliemehran021@gmail.com>
2024-05-23 18:11:26 +0500

    add tailwand

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.header');
+<h1 class="bg-blue-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem qui facilis, excepturi ipsam
+    dolore cumque cupiditate aliquid culpa sunt, accusantium odit aspernatur corporis a voluptas id veritatis harum
+    quidem quasi.</h1>
+    @extends('layouts.footer');
