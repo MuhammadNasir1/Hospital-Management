c45ee881e10811c516f49e3d9ddf8776b22575be
mehran02111 <charliemehran021@gmail.com>
2024-05-28 15:45:02 +0500

    dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.header')
+@extends('layouts.nav')
+@section('content')
+
+
+<div class="btns flex p-8 justify-around">
+
+    <div class="btn1 flex flex-col items-center justify-items-starth-[120px] w-[332px] border-2 rounded-lg  bg-[#8CD5A7] opacity-[25%]">
+
+
+
+
+
+
+    </div>
+    <div class="btn2  h-[120px] w-[332px] border-2 rounded-lg bg-[#7CC6D6] opacity-[25%]">2</div>
+    <div class="btn3  h-[120px] w-[332px] border-2 rounded-lg bg-[#858C94] opacity-[25%] ">3</div>
+</div>
+
+
+
+
+
+
+
+
+@endsection
+@extends('layouts.footer')
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -268,7 +268,7 @@
         <ul class="space-y-2 font-medium">
 
             <li>
-                <a href="#"
+                <a href="{{route('dashboard')}}"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                         aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
@@ -346,6 +346,7 @@
 </aside>
 
 <div class="p-4 sm:ml-64">
+
     <div class="  mt-14">
 
 
